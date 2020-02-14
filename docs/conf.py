93dc0c604190f1cece2f97400373cec9bec3370c
jfallmann <fall@bioinf.uni-leipzig.de>
2020-02-14 13:59:59 +0100

    restructured sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ release = '01.03.2020'
 # -- General configuration ---------------------------------------------------
 #Master file to be generated
 
-master_doc = 'NextSnake'
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
