a163d944f24e7003482994d3708bef2945866dac
Robin Goldmann <robin@bioinf.uni-leipzig.de>
2020-03-04 16:10:32 +0100

    edger

--- scripts/Analysis/DE/EdgeR.R
+++ scripts/Analysis/DE/EdgeR.R
@@ -10,9 +10,6 @@ smp <- args[2]
 drct <- args[3]
 cmp <- args[4]
 
-for(i in 1:(length(cmp))){
-  print(i)
-}
 
 f <- function(list){
   spektrum <- rainbow(max(list),alpha=1)
@@ -23,24 +20,29 @@ f <- function(list){
   return(cl)
 }
 
-dir.create(file.path(drct,'PICS'), showWarnings = FALSE)
+dir.create(file.path(drct,'RESULTS'), showWarnings = FALSE)
 
 ###READ IN
 data <- read.table(tbl,skip=1,h=F,sep="\t",check.names=FALSE)
+header <- strsplit(readLines(tbl, n=1),"\t")[[1]]
+header[1] <- "gene"
+comparison <- strsplit(strsplit(cmp, ",")[[1]], ":")
+#######FILTER COLOUMS WITH ONLY NA
+data <- Filter(function(x) !all(is.na(x)), data)
+colnames(data) <- header
+
 samples <- read.table(smp,h=F,sep="\t")
 colnames(samples) <- c("name","group")
+
 ###FIND GROUPS
-groups <- as.numeric(samples$group)
+groups_by_number <- as.numeric(samples$group)
 groups_by_name <- samples$group
 
 ###GET GENES
 l <- strsplit(as.character(data$V1), ":")
 genes <- unlist(lapply(l,function(x) x[length(x)]))
 
-###FILTER COLOUMS WITH ONLY NA
-data <- Filter(function(x) !all(is.na(x)), data)
-
-###GET RID OF ID COLUMN
+###GET RID OF "GENE" COLUMN
 counts <- data[,-c(1)]
 
 ###CONVERT other NA TO 0
@@ -64,12 +66,12 @@ tmm <- as.data.frame(cpm(dge))
 colnames(tmm) <- t(dge$samples$samples)
 tmm$ID <- dge$genes$genes
 tmm <- tmm[c(ncol(tmm),1:ncol(tmm)-1)]
-write.table(tmm, file=paste(drct,"normalized_table.tsv",sep=''), sep="\t", quote=F, row.names=FALSE)
+write.table(tmm, file=paste(drct,"normalized_table.tsv",sep='/'), sep="\t", quote=F, row.names=FALSE)
 
 ###plot MDS
-out <- paste(tbl,"_MDS",".png",sep="")
+out <- paste(drct,"RESULTS","MDS.png",sep="/")
 png(out, width = 1000, height = 800)
-colors <- f(groups)
+colors <- f(groups_by_number)
 plotMDS(dge,col=colors)
 dev.off()
 
@@ -79,12 +81,11 @@ colnames(design) <- levels(dge$samples$group)
 
 #design
 ###Estimate dispersion
-
 dge <- estimateDisp(dge, design, robust=TRUE)
 
 ###plot Dispersion
 
-out <- paste(drct,"BCV",".png",sep="")
+out <- paste(drct,"RESULTS","BCV.png",sep="/")
 png(out, width = 350, height = 350)
 plotBCV(dge)
 dev.off()
@@ -93,14 +94,50 @@ dev.off()
 ####fit glms
 fit <- glmFit(dge, design)
 
-###COMPARE ALL COMBINATIONS
+###IF NO COMPARE ENTRY - compare all vs all
 
-"""
-for(i in 1:(dim(design)[2]-1)){
-  for(j in (i+1):dim(design)[2]){
+if(cmp == ''){
+
+  for(i in 1:(dim(design)[2]-1)){
+    for(j in (i+1):dim(design)[2]){
+      con <- integer(dim(design)[2])
+      con[i] <- -1
+      con[j] <- 1
+
+      ####likelihood ratiotest
+      lrt <- glmLRT(fit, contrast = con)
+
+      ####top DE
+      #topTags(lrt)
+
+      ###DE distribution
+      #summary(decideTests(lrt))
+
+      ###plot lFC vs CPM
+      a <- levels(dge$samples$group)[i]
+      b <- levels(dge$samples$group)[j]
+      cs <- paste("rand",a,"vs",b,sep="-")
+      title <- paste(a, " vs. ",b)
+
+      print(paste("compare ", title))
+
+      out <- paste(drct,"/RESULTS/",cs,".png",sep="")
+      png(out, width = 350, height = 350)
+      plotMD(lrt, main=title)
+      abline(h=c(-1, 1), col="blue")
+      dev.off()
+    }
+  }
+
+###ELSE - compare selected Groups
+} else {
+
+  contrast_list = list()
+  for(i in 1:length(comparison)){
     con <- integer(dim(design)[2])
-    con[i] <- -1
-    con[j] <- 1
+    m <- match(comparison[[i]], colnames(design))
+    con[m[1]] <- -1
+    con[m[2]] <- 1
 
     ####likelihood ratiotest
     lrt <- glmLRT(fit, contrast = con)
@@ -112,18 +149,17 @@ for(i in 1:(dim(design)[2]-1)){
     #summary(decideTests(lrt))
 
     ###plot lFC vs CPM
-    a <- levels(dge$samples$group)[i]
-    b <- levels(dge$samples$group)[j]
-    cs <- paste("_",a,"=",b,sep="")
+    a <- comparison[[i]][1]
+    b <- comparison[[i]][2]
+    cs <- paste(a,"vs",b,sep="-")
     title <- paste(a, " vs. ",b)
 
     print(paste("compare ", title))
 
-    out <- paste(drct,"/PICS/",cs,".png",sep="")
+    out <- paste(drct,"/RESULTS/",cs,".png",sep="")
     png(out, width = 350, height = 350)
     plotMD(lrt, main=title)
     abline(h=c(-1, 1), col="blue")
     dev.off()
   }
 }
-"""
