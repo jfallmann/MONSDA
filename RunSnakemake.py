9ebb846a941a56f09aa9ecf272847b1aed2d440c
meisterL <39379276+meisterL@users.noreply.github.com>
2020-04-07 12:26:42 +0200

    back

--- RunSnakemake.py
+++ RunSnakemake.py
@@ -8,9 +8,9 @@
 # Created: Mon Feb 10 08:09:48 2020 (+0100)
 # Version:
 # Package-Requires: ()
-# Last-Updated: Wed Apr  1 15:25:17 2020 (+0200)
+# Last-Updated: Thu Mar 26 17:47:14 2020 (+0100)
 #           By: Joerg Fallmann
-#     Update #: 806
+#     Update #: 804
 # URL:
 # Doc URL:
 # Keywords:
@@ -267,8 +267,8 @@ def run_snakemake (configfile, debugdag, filegraph, workdir, useconda, procs, sk
                 if 'QC' in subworkflows and 'QC' in config:
                     makeoutdir('QC')
                     if 'MAPPING' in subworkflows:
-                        #with open(smko, 'a') as smkout:
-                        #    smkout.write('rule themall:\n\tinput: expand("DONE/{file}_mapped",file=samplecond(SAMPLES,config))\n\n')
+                        with open(smko, 'a') as smkout:
+                            smkout.write('rule themall:\n\tinput: expand("DONE/{file}_mapped",file=samplecond(SAMPLES,config))\n\n')
 
                         smkf = os.path.abspath(os.path.join('snakes','workflows','multiqc.smk'))
                         with open(smko, 'a') as smkout:
@@ -287,7 +287,7 @@ def run_snakemake (configfile, debugdag, filegraph, workdir, useconda, procs, sk
 
                 subconf = NestedDefaultDict()
                 for subwork in subworkflows:
-                    log.info(logid+'PREPARING '+str(subwork)+' FOR CONDITION '+str(condition))
+                    log.info(logid+'PREPARING '+str(subwork))
                     listoftools, listofconfigs = create_subworkflow(config, subwork, [condition])
                     for i in range(0,len(listoftools)):
                         toolenv, toolbin = map(str,listoftools[i])
@@ -459,24 +459,25 @@ def runjob(jobtorun):
         #return subprocess.run(jobtorun, shell=True, universal_newlines=True, capture_output=True)  # python >= 3.7
         job = subprocess.Popen(jobtorun, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
 
-        status = job.wait()
-        output = str.join('',job.stdout.readlines()).rstrip()
-        err = str.join('',job.stderr.readlines()).rstrip()
-        if output:
-            log.info(logid+str(output))
-            if any(x in output for x in ['ERROR','Error','error','Exception']) and not 'Workflow finished' in output:
-                log.error(logid+'STOPOUT: '+str(err))
-                job.kill()
-                sys.exit(output)
-        if err:
-            if not 'Workflow finished' in err and not 'Nothing to be done' in err and any(x in err for x in ['ERROR','Error','error','Exception']):
-                log.error(logid+'STOPERROR: '+str(err))
-                job.kill()
-                sys.exit(err)
-            else:
-                log.info(logid+str(err))
-        if status is not None:
-            break
+        while True:
+            status = job.poll()
+            output = str.join('',job.stdout.readlines()).rstrip()
+            err = str.join('',job.stderr.readlines()).rstrip()
+            if output:
+                log.info(logid+str(output))
+                if any(x in output for x in ['ERROR','Error','error','Exception']) and not 'Workflow finished' in output:
+                    log.error(logid+'STOPOUT: '+str(err))
+                    job.kill()
+                    sys.exit(output)
+            if err:
+                if not 'Workflow finished' in err and not 'Nothing to be done' in err and any(x in err for x in ['ERROR','Error','error','Exception']):
+                    log.error(logid+'STOPERROR: '+str(err))
+                    job.kill()
+                    sys.exit(err)
+                else:
+                    log.info(logid+str(err))
+            if status is not None:
+                break
 
         return status
 
