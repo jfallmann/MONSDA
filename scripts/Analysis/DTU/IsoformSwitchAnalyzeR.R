7202c9e54d2f383f68f1aa583c3dbbd1f7f78c69
jfallmann <fallmann.joerg@gmail.com>
2020-08-25 17:50:20 +0200

    update

--- scripts/Analysis/DTU/IsoformSwitchAnalyzeR.R
+++ scripts/Analysis/DTU/IsoformSwitchAnalyzeR.R
@@ -58,101 +58,257 @@ for(contrast in comparisons[[1]]){
   
   tryCatch({
     
-  print(contrast_groups)
-  cf <- grep(paste(contrast_groups,collapse="|"),countfiles,value=TRUE)
-  des <- droplevels(design[design$condition %in% contrast_groups,])
+    print(contrast_groups)
+    cf <- grep(paste(contrast_groups,collapse="|"),countfiles,value=TRUE)
+    des <- droplevels(design[design$condition %in% contrast_groups,])
   
-  ### Import Salmon data
-  salmonQuant <- importIsoformExpression(
-      sampleVector = cf,
-      addIsofomIdAsColumn = TRUE
-  )
+    ### Import Salmon data
+    salmonQuant <- importIsoformExpression(
+        sampleVector = cf,
+        addIsofomIdAsColumn = TRUE
+    )
 
-  ### Create switchAnalyzeRlist
-  SwitchList <- importRdata(
-    isoformCountMatrix   = salmonQuant$counts,
-    isoformRepExpression = salmonQuant$abundance,
-    designMatrix         = des,
-    isoformExonAnnoation = annotation,
-    isoformNtFasta       = fasta,
-    showProgress = TRUE
-  )
+    ### Create switchAnalyzeRlist
+    SwitchList <- importRdata(
+      isoformCountMatrix   = salmonQuant$counts,
+      isoformRepExpression = salmonQuant$abundance,
+      designMatrix         = des,
+      isoformExonAnnoation = annotation,
+      isoformNtFasta       = fasta,
+      showProgress = TRUE
+    )
 
-  ###Part 1 of Analysis
-  ####Filter
-  SwitchListFiltered <- preFilter(
-    switchAnalyzeRlist = SwitchList,
-    geneExpressionCutoff = 10,
-    isoformExpressionCutoff = 3,
-    removeSingleIsoformGenes = TRUE
-  )
+    ###Part 1 of Analysis
+    ####Filter
+    SwitchListFiltered <- preFilter(
+      switchAnalyzeRlist = SwitchList,
+      geneExpressionCutoff = 10,
+      isoformExpressionCutoff = 3,
+      removeSingleIsoformGenes = TRUE
+    )
+    
+    ###Analyze with DEXSeq
+    SwitchListDEX <- isoformSwitchTestDEXSeq(
+      switchAnalyzeRlist = SwitchListFiltered,
+      alpha = 0.05,
+      dIFcutoff = 0.1,
+      correctForConfoundingFactors=TRUE,
+      overwriteIFvalues=TRUE,
+      reduceToSwitchingGenes = TRUE,
+      reduceFurtherToGenesWithConsequencePotential = TRUE,
+      onlySigIsoforms = FALSE,
+      showProgress = TRUE,
+      quiet = FALSE
+    )
+    
+    extractSwitchSummary( SwitchListDEX )
+    
+    ###Analyze with DRIMSeq
+    SwitchListDRIM <- isoformSwitchTestDRIMSeq(
+      switchAnalyzeRlist = SwitchListFiltered,
+      alpha = 0.05,
+      dIFcutoff = 0.1,
+      testIntegration = 'intersect',
+      reduceToSwitchingGenes = TRUE,
+      reduceFurtherToGenesWithConsequencePotential = TRUE,
+      onlySigIsoforms = FALSE,
+      dmFilterArgs=list(
+        min_feature_expr = 4,
+        min_samps_feature_expr = min(
+          SwitchListFiltered$conditions$nrReplicates
+        )
+      ),
+      showProgress = TRUE,
+      quiet = FALSE
+    )
+    
+    extractSwitchSummary( SwitchListDRIM )
   
-  ###Analyze with DEXSeq
-  SwitchListDEX <- isoformSwitchTestDEXSeq(
-    switchAnalyzeRlist = SwitchListFiltered,
-    reduceToSwitchingGenes=FALSE
-  )
+    ###Analyze ORFs
+    SwitchListDEX <- analyzeORF(
+      SwitchListDEX,
+      genomeObject = NULL,
+      minORFlength=100,
+      orfMethod = "longest",
+      cds = NULL,
+      PTCDistance = 50,
+      startCodons="ATG",
+      stopCodons=c("TAA", "TAG", "TGA"),
+      showProgress=TRUE,
+      quiet=FALSE
+    )
   
-  extractSwitchSummary( SwitchListDEX )
+    SwitchListDRIM <- analyzeORF(
+      SwitchListDRIM,
+      genomeObject = NULL,
+      minORFlength=100,
+      orfMethod = "longest",
+      cds = NULL,
+      PTCDistance = 50,
+      startCodons="ATG",
+      stopCodons=c("TAA", "TAG", "TGA"),
+      showProgress=TRUE,
+      quiet=FALSE
+    )
+    
+    ###Extract Sequences
+    SwitchListDEX <- extractSequence(
+      SwitchListDEX, 
+      onlySwitchingGenes = TRUE,
+      alpha = 0.05,
+      dIFcutoff = 0.1,
+      extractNTseq = TRUE,
+      extractAAseq = TRUE,
+      removeShortAAseq = TRUE,
+      removeLongAAseq  = FALSE,
+      alsoSplitFastaFile = FALSE,
+      removeORFwithStop=TRUE,
+      addToSwitchAnalyzeRlist = TRUE,
+      pathToOutput = outdir,
+      outputPrefix = paste("DEXSwitch_",contrast_name,sep=""),
+      writeToFile = TRUE
+    )
+    
+    SwitchListDRIM <- extractSequence(
+      SwitchListDRIM, 
+      onlySwitchingGenes = TRUE,
+      alpha = 0.05,
+      dIFcutoff = 0.1,
+      extractNTseq = TRUE,
+      extractAAseq = TRUE,
+      removeShortAAseq = TRUE,
+      removeLongAAseq  = FALSE,
+      alsoSplitFastaFile = FALSE,
+      removeORFwithStop=TRUE,
+      addToSwitchAnalyzeRlist = TRUE,
+      pathToOutput = outdir,
+      outputPrefix = paste("DRIMSwitch_",contrast_name,sep=""),
+      writeToFile = TRUE
+    )
+    
+    
+    ###Alternative Splicing
+    SwitchListDEX <- analyzeAlternativeSplicing(
+      switchAnalyzeRlist = SwitchListDEX,
+      onlySwitchingGenes=TRUE,
+      alpha=0.05,
+      dIFcutoff = 0.1,
+      showProgress=TRUE
+    )
+    
+    SwitchListDRIM <- analyzeAlternativeSplicing(
+      switchAnalyzeRlist = SwitchListDRIM,
+      onlySwitchingGenes=TRUE,
+      alpha=0.05,
+      dIFcutoff = 0.1,
+      showProgress=TRUE
+    )
+    
+    #### Global splicing analysis
+    pdf(paste("IsoformSwitchR_DEX",contrast_name,"SplicingSummary.pdf",sep="_"))
+    extractSplicingSummary( SwitchListDEX )
+    dev.off()
+    pdf(paste("IsoformSwitchR_DEX",contrast_name,"SplicingEnrichment.pdf",sep="_"))
+    extractSplicingEnrichment( SwitchListDEX )
+    dev.off()
+    pdf(paste("IsoformSwitchR_DEX",contrast_name,"SplicingGenomeWide.pdf",sep="_"))
+    extractSplicingGenomeWide( SwitchListDEX )
+    dev.off()
+    
+    pdf(paste("IsoformSwitchR_DRIM",contrast_name,"SplicingSummary.pdf",sep="_"))
+    extractSplicingSummary( SwitchListDRIM )
+    dev.off()
+    pdf(paste("IsoformSwitchR_DRIM",contrast_name,"SplicingEnrichment.pdf",sep="_"))
+    extractSplicingEnrichment( SwitchListDRIM )
+    dev.off()
+    pdf(paste("IsoformSwitchR_DRIM",contrast_name,"SplicingGenomeWide.pdf",sep="_"))
+    extractSplicingGenomeWide( SwitchListDRIM )
+    dev.off()
+    
+    
+    ###IntronRetention
+    SwitchListDEX <- analyzeIntronRetention(
+      SwitchListDEX,
+      onlySwitchingGenes = TRUE,
+      alpha = 0.05,
+      dIFcutoff = 0.1,
+      showProgress = TRUE,
+      quiet = FALSE
+    )
+    
+    SwitchListDRIM <- analyzeIntronRetention(
+      SwitchListDRIM,
+      onlySwitchingGenes = TRUE,
+      alpha = 0.05,
+      dIFcutoff = 0.1,
+      showProgress = TRUE,
+      quiet = FALSE
+    )
+    
+    ###Get biological meaning
+    consequencesOfInterest <- c('intron_retention','NMD_status','ORF_seq_similarity','tss','tts','intron_structure')
+    
+    SwitchListDEX <- analyzeSwitchConsequences(
+      SwitchListDEX,
+      consequencesToAnalyze = consequencesOfInterest,
+      alpha=0.05,
+      dIFcutoff=0.1,
+      onlySigIsoforms=TRUE,
+      ntCutoff=50,
+      ntFracCutoff=NULL,
+      ntJCsimCutoff=0.8,
+      AaCutoff=10,
+      AaFracCutoff=0.5,
+      AaJCsimCutoff=0.9,
+      removeNonConseqSwitches=TRUE,
+      showProgress=TRUE
+    )
+    
+    SwitchListDRIM <- analyzeSwitchConsequences(
+      SwitchListDRIM,
+      consequencesToAnalyze = consequencesOfInterest,
+      alpha=0.05,
+      dIFcutoff=0.1,
+      onlySigIsoforms=TRUE,
+      ntCutoff=50,
+      ntFracCutoff=NULL,
+      ntJCsimCutoff=0.8,
+      AaCutoff=10,
+      AaFracCutoff=0.5,
+      AaJCsimCutoff=0.9,
+      removeNonConseqSwitches=TRUE,
+      showProgress=TRUE
+    )
   
-  SwitchListDRIM <- isoformSwitchTestDRIMSeq(
-    switchAnalyzeRlist = SwitchListFiltered,
-    alpha = 0.05,
-    dIFcutoff = 0.1,
-    testIntegration = 'intersect',
-    reduceToSwitchingGenes = TRUE,
-    reduceFurtherToGenesWithConsequencePotential = TRUE,
-    onlySigIsoforms = FALSE,
-    dmFilterArgs=list(
-      min_feature_expr = 4,
-      min_samps_feature_expr = min(
-        SwitchListFiltered$conditions$nrReplicates
+    ###Summarize
+    
+    
+    topdex <- extractTopSwitches(
+      SwitchListDEX,
+      filterForConsequences = TRUE,
+      extractGenes=TRUE,
+      alpha=0.05,
+      dIFcutoff = 0.1,
+      n=extractSwitchSummary(SwitchListDEX)$nrIsoforms,
+      inEachComparison=TRUE,
+      sortByQvals=TRUE 
       )
-    ),
-    showProgress = TRUE,
-    quiet = FALSE
-  )
-  
-  extractSwitchSummary( SwitchListDRIM )
-  
-  ORFDEX <- analyzeORF(
-    SwitchListDEX,
-    genomeObject = NULL,
-    minORFlength=100,
-    orfMethod = "longest",
-    cds = NULL,
-    PTCDistance = 50,
-    startCodons="ATG",
-    stopCodons=c("TAA", "TAG", "TGA"),
-    showProgress=TRUE,
-    quiet=FALSE
-  )
-  
-  ORFDRIM <- analyzeORF(
-    SwitchListDRIM,
-    genomeObject = NULL,
-    minORFlength=100,
-    orfMethod = "longest",
-    cds = NULL,
-    PTCDistance = 50,
-    startCodons="ATG",
-    stopCodons=c("TAA", "TAG", "TGA"),
-    showProgress=TRUE,
-    quiet=FALSE
-  )
-  
-  SwitchListDEXout <- extractSequence(
-    SwitchListDEX, 
-    pathToOutput = paste(output,paste("DEXSwitch_",contrast_name,sep=""),sep="/"),
-    writeToFile = TRUE
-  )
-  
-  SwitchListDRIMout <- extractSequence(
-    SwitchListDRIM, 
-    pathToOutput = paste(output,paste("DRIMSwitch_",contrast_name,sep=""),sep="/"),
-    writeToFile = TRUE
-  )
-  
+    csvout <- paste('IsoformSwitchR_DEX_',contrast_name,'.tsv.gz', sep='')
+    write.table(as.data.frame(topdex), gzfile(csvout), sep="\t", row.names=FALSE, quote=F)
+    
+    topdrim <- extractTopSwitches(
+      SwitchListDRIM,
+      filterForConsequences = TRUE,
+      extractGenes=TRUE,
+      alpha=0.05,
+      dIFcutoff = 0.1,
+      n=extractSwitchSummary(SwitchListDRIM)$nrIsoforms,
+      inEachComparison=TRUE,
+      sortByQvals=TRUE 
+    )
+    csvout <- paste('IsoformSwitchR_DRIM_',contrast_name,'.tsv.gz', sep='')
+    write.table(as.data.frame(topdex), gzfile(csvout), sep="\t", row.names=FALSE, quote=F)
+    
   }, error=function(e){
     rm(res,resOrdered)
     file.create(paste("IsoformSwitchAnalyseR_",contrast_name,'.tsv.gz',sep=""))
