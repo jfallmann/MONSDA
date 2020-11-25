c9928059d8e5380908a1eae776fe4d33926eee1f
jfallmann <fallmann.joerg@gmail.com>
2020-11-25 11:11:02 +0100

    allmap again

--- RunSnakemake.py
+++ RunSnakemake.py
@@ -8,9 +8,9 @@
 # Created: Mon Feb 10 08:09:48 2020 (+0100)
 # Version:
 # Package-Requires: ()
-# Last-Updated: Mon Nov 23 13:41:17 2020 (+0100)
+# Last-Updated: Wed Nov 25 11:10:53 2020 (+0100)
 #           By: Joerg Fallmann
-#     Update #: 1114
+#     Update #: 1115
 # URL:
 # Doc URL:
 # Keywords:
@@ -332,8 +332,8 @@ def run_snakemake (configfile, debugdag, filegraph, workdir, useconda, procs, sk
                     makeoutdir('QC')
                     if 'MAPPING' in subworkflows:
                         with open(smko, 'a') as smkout:
-                            #smkout.write('rule themall:\n\tinput:\t'+allmap+',\n\t\t'+allqc+'\n\n')
-                            smkout.write('rule themall:\n\tinput:\t'+allqc+'\n\n')
+                            smkout.write('rule themall:\n\tinput:\t'+allmap+',\n\t\t'+allqc+'\n\n')
+                            #smkout.write('rule themall:\n\tinput:\t'+allqc+'\n\n')
                     else:
                         if 'TRIMMING' in subworkflows and 'DEDUP' not in subworkflows:
                             with open(smko, 'a') as smkout:
