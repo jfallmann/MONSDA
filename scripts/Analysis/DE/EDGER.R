60a58d21356d38c4da9221555fc2b98688fbb4ee
jfallmann <fallmann.joerg@gmail.com>
2021-03-17 11:18:31 +0100

    DE norm testing

--- scripts/Analysis/DE/EDGER.R
+++ scripts/Analysis/DE/EDGER.R
@@ -62,31 +62,12 @@ if (combi == "none"){
 ## readin counttable
 countData <- read.table(countfile,header = TRUE,row.names=1)
 
-#Check if names are consistent
+# Check if names are consistent
 if (!all(rownames(sampleData) %in% colnames(countData))){
     stop("Count file does not correspond to the annotation file")
 }
 
-## get genes names out
-genes <- rownames(countData)
-dge <- DGEList(counts=countData, group=groups, samples=samples, genes=genes)
-
-## filter low counts
-keep <- filterByExpr(dge)
-dge <- dge[keep, , keep.lib.sizes=FALSE]
-
-# Normalize by spike in if available
-print("Spike-in used, data will be normalized to spike in separately")
-if (spike != ''){
-    ctrlgenes <- readLines(spike)
-    dge_norm <- estimateSizeFactors(dds, controlGenes=ctrlgenes)
-}
-
-
-## normalize with TMM
-dge <- calcNormFactors(dge, method = "TMM", BPPARAM=BPPARAM)
-
-##name types and levels for design
+## name types and levels for design
 bl <- sapply("batch",paste0,levels(batches)[-1])
 tl <- sapply("type",paste0,levels(types)[-1])
 
@@ -110,6 +91,71 @@ if (length(levels(types)) > 1){
 }
 
 print(paste('FITTING DESIGN: ',design, sep=""))
+
+
+## get genes names and spike-ins
+
+genes <- rownames(countData)
+if (spike != ''){
+    ctrlgenes <- readLines(spike)
+    countData <- countData %>% filter(row.names(countData) %!in% ctrlgenes)
+    genes <- rownames(countData)
+    counts_norm <-RUVg(countData, ctrlgenes, k=1)
+
+    dge_norm <- DGEList(counts=counts(counts_norm), group=groups, samples=samples, genes=genes)
+
+    ## filter low counts
+    keep <- filterByExpr(dge_norm)
+    dge_norm <- dge_norm[keep, , keep.lib.sizes=FALSE]
+
+    ## normalize with TMM
+    dge_norm <- calcNormFactors(dge_norm, method = "TMM", BPPARAM=BPPARAM)
+
+
+    ## create file normalized table
+    tmm_norm <- as.data.frame(cpm(dge_norm))
+    colnames(tmm_norm) <- t(dge_norm$samples$samples)
+    tmm_norm$ID <- dge_norm$genes$genes
+    tmm_norm <- tmm_norm[c(ncol(tmm_norm),1:ncol(tmm_norm)-1)]
+
+    setwd(outdir)
+    write.table(as.data.frame(tmm_norm), gzfile(paste("Tables/    DE","EDGER",combi,"DataSet","table","AllConditionsNormalized_norm.tsv.gz",sep="_")), sep="\t", quote=F, row.names=FALSE)
+
+    ## create file MDS-plot with and without summarized replicates
+    out <- paste("Figures/    DE","EDGER",combi,"DataSet","figure","AllConditionsMDS_norm.png", sep="_")
+    png(out)
+    plotMDS(dge_norm, col = as.numeric(dge_norm$samples$group), cex = 1)
+    dev.off()
+
+    ## estimate Dispersion
+    dge_norm <- estimateDisp(dge_norm, design, robust=TRUE)
+
+    ## create file BCV-plot - visualizing estimated dispersions
+    out <- paste("Figures/    DE","EDGER",combi,"DataSet","figure","AllConditionsBCV_norm.png", sep="_")
+    png(out)
+    plotBCV(dge_norm)
+    dev.off()
+
+    ## fitting a quasi-likelihood negative binomial generalized log-linear model to counts
+    fit_norm <- glmQLFit(dge_norm, design, robust=TRUE)
+
+    ## create file quasi-likelihood-dispersion-plot
+    out <- paste("Figures/    DE","EDGER",combi,"DataSet","figure","AllConditionsQLDisp_norm.png", sep="_")
+    png(out)
+    plotQLDisp(fit_norm)
+    dev.off()
+
+ }
+
+# Same without spike-in normalization
+dge <- DGEList(counts=countData, group=groups, samples=samples, genes=genes)
+
+## filter low counts
+keep <- filterByExpr(dge)
+dge <- dge[keep, , keep.lib.sizes=FALSE]
+
+## normalize with TMM
+dge <- calcNormFactors(dge, method = "TMM", BPPARAM=BPPARAM)
 
 
 ## create file normalized table
@@ -170,12 +216,7 @@ for(compare in comparisons[[1]]){
         }
         contrast <- as.numeric(contrast[,1])
 
-
-        # # reduce data for testing
-        # fit <- fit[1:250,]
-
-        # TESTING
-        # lrt <- glmLRT(fit, contrast=contrast) ## likelihood-ratiotest
+        # Testing
         qlf <- glmQLFTest(fit, contrast=contrast) ## glm quasi-likelihood-F-Test
 
         # add comp object to list for image
@@ -211,8 +252,49 @@ for(compare in comparisons[[1]]){
         abline(h=c(-1, 1), col="blue")
         dev.off()
 
+
+        if (spike != ''){  # Same for spike-in normalized
+            rm(qlf, tops, res)
+            # Testing
+            qlf <- glmQLFTest(fit_norm, contrast=contrast) ## glm quasi- likelihood-F-Test
+
+            # add comp object to list for image
+            comparison_objs <- append(comparison_objs, qlf)
+
+            # # Add gene names  (check how gene_id col is named )
+            qlf$table$Gene  <- lapply(rownames(qlf$table) ,     function(x){get_gene_name(x,gtf.df)})
+            qlf$table$Gene_ID <- rownames(qlf$table)
+            res <- qlf$table[,c(6,5,1,2,3,4)]
+            res <- as.data.frame(apply(res,2,as.character))
+
+            # create results table
+            write.table(as.data.frame(res), gzfile(paste("Tables/    DE","EDGER",combi,contrast_name,"table","results_norm.tsv.gz", sep="_")), sep="\t", quote=F, row.names=FALSE)
+
+            # create sorted results Tables
+            tops <- topTags(qlf, n=nrow(qlf$table), sort.by="logFC")
+            tops$table$Gene  <- lapply(rownames(tops) ,     function(x){get_gene_name(x,gtf.df)})
+            tops$table$Gene_ID <- rownames(tops$table)
+            tops <- tops$table[,c(8,7,2,3,4,5,6)]
+            tops <- as.data.frame(apply(tops,2,as.character))
+            write.table(tops, file=paste("Tables/    DE","EDGER",combi,contrast_name,"table","resultsLogFCsorted_norm.tsv.gz", sep="_"), sep="\t", quote=F, row.names=FALSE)
+            tops <- topTags(qlf, n=nrow(qlf$table), sort.by="PValue")
+            tops$table$Gene  <- lapply(rownames(tops) ,     function(x){get_gene_name(x,gtf.df)})
+            tops$table$Gene_ID <- rownames(tops$table)
+            tops <- tops$table[,c(8,7,2,3,4,5,6)]
+            tops <- as.data.frame(apply(tops,2,as.character))
+            write.table(tops, file=paste("Tables/    DE","EDGER",combi,contrast_name,"table","resultsPValueSorted_norm.tsv.gz", sep="_"), sep="\t", quote=F, row.names=FALSE)
+
+            ## plot lFC vs CPM
+            out <- paste("Figures/    DE","EDGER",combi,contrast_name,"figure","MD_norm.png",sep="_")
+            png(out)
+            plotMD(qlf, main=contrast_name)
+            abline(h=c(-1, 1), col="blue")
+            dev.off()
+
+        }
+
         # cleanup
-        rm(qlf, BPPARAM)
+        rm(qlf, res, tops, BPPARAM)
         print(paste('cleanup done for ', contrast_name, sep=''))
     })
 }
