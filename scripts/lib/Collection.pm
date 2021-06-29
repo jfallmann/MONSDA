59261f7027b063f8a87862a403d0f4c3afb13688
jfallmann <fallmann.joerg@gmail.com>
2021-06-29 09:25:24 +0200

    typo

--- scripts/lib/Collection.pm
+++ scripts/lib/Collection.pm
@@ -2245,7 +2245,7 @@ sub bed_to_coverage{
 				@pro = split(/\|/,$name) ;
 			}
             elsif($name eq 'X'){ #Special case for piranha
-                $peakwidth = $cend-$cstart;
+                my $peakwidth = $cend-$cstart;
                 for (1..$peakwidth){
                     push @pro, $_.":".nearest(1,$score/$peakwidth);
                 }
