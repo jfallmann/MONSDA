7dc57df4a85935b16f40dff716e3249921d18e98
jfallmann <fallmann.joerg@gmail.com>
2021-10-13 08:27:59 +0200

    working on setup datafiles

--- setup.py
+++ setup.py
@@ -9,17 +9,31 @@ DESCRIPTION = "NextSnakes, a modular assembler of snakemake and nexflow workflow
 # Set __version__ done by versioneer
 # exec(open("NextSnakes/__init__.py").read())
 
-scripts = list()
-for s in glob("scripts/**", recursive=True):
-    if any(x in s for x in [".pl", ".py", ".sh"]):
-        scripts.append(os.path.relpath(s))  # os.path.join(s, os.path.split(s)[1]))
-workflows = list()
-for d in glob("workflows/*"):
-    if not "wip" in d:
-        workflows.append(os.path.relpath(d))  # os.path.join(d, os.path.split(d)[1]))
-envs = list()
-for e in glob("envs/*"):
-    envs.append(os.path.relpath(e))  # os.path.join(d, os.path.split(d)[1]))
+def generate_datafiles():
+    df = list()
+    scripts = list()
+    for s in glob("scripts/**", recursive=True):
+        if any(x in s for x in [".pl", ".py", ".sh"]):
+            scripts.append(os.path.relpath(s))  # os.path.join(s, os.path.split(s)[1]))
+    for s in scripts:
+        df.append((os.path.join("share", "NextSnakes", os.path.dirname(s)), [os.path.basename(s)]))
+
+    workflows = list()
+    for d in glob("workflows/*"):
+        if not "wip" in d:
+            workflows.append(os.path.relpath(d))  # os.path.join(d, os.path.split(d)[1]))
+    for w in workflows:
+        df.append((os.path.join("share", "NextSnakes", os.path.dirname(w)), [os.path.basename(w)]))
+
+    envs = list()
+    for e in glob("envs/*"):
+        envs.append(os.path.relpath(e))  # os.path.join(d, os.path.split(d)[1]))
+    for e in envs:
+        df.append((os.path.join("share", "NextSnakes", os.path.dirname(e)), [os.path.basename(e)]))
+
+    df.append(("", ["LICENSE"]))
+    print(df)
+    return(df)
 
 requires = open("requirements.txt").read().strip().split("\n")
 
@@ -33,12 +47,7 @@ setup(
     packages=find_packages(include=["NextSnakes", "NextSnakes.*"]),
     include_package_data=True,
     # scripts=scripts,
-    data_files=[
-        (os.path.join("share", "NextSnakes", "workflows"), workflows),
-        (os.path.join("share", "NextSnakes", "scripts"), scripts),
-        (os.path.join("share", "NextSnakes", "envs"), envs),
-        ("", ["LICENSE"]),
-    ],
+    data_files=generate_datafiles(),
     entry_points={
         "console_scripts": [
             "NextSnakes = NextSnakes.RunNextSnakes:main",
