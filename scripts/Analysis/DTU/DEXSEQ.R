1b63bc1899637cc84a2cfdfa81b2aa976eda1ff4
jfallmann <fallmann.joerg@gmail.com>
2023-03-10 11:53:04 +0100

    DEXSEQ wip

--- scripts/Analysis/DTU/DEXSEQ.R
+++ scripts/Analysis/DTU/DEXSEQ.R
@@ -7,29 +7,29 @@ suppressPackageStartupMessages({
     library(rtracklayer)
 })
 
-options(echo=TRUE)
+options(echo = TRUE)
 
 ### ARGS
 args <- commandArgs(trailingOnly = TRUE)
 argsLen <- length(args)
-anname  <- args[1]
-gtf     <- args[2]
-outdir  <- args[3]
-combi   <- args[4]
-cmp     <- args[5]
-cores   <- as.integer(args[6])
+anname <- args[1]
+gtf <- args[2]
+outdir <- args[3]
+combi <- args[4]
+cmp <- args[5]
+cores <- as.integer(args[6])
 filter <- if (argsLen > 6) args[7] else "min_samps_feature_expr = n.small, min_feature_expr = 10, min_samps_feature_prop = n.small, min_feature_prop = 0.1, min_samps_gene_expr = n, min_gene_expr = 10"
 print(args)
 
 
 ### FUNCS
-get_gene_name <- function(id, df){
-    name_list <- df$gene_name[df['gene_id'] == id]
-    if(length(unique(name_list)) == 1){
+get_gene_name <- function(id, df) {
+    name_list <- df$gene_name[df["gene_id"] == id]
+    if (length(unique(name_list)) == 1) {
         return(name_list[1])
-    }else{
-        message(paste("WARNING: ambigous gene id: ",id))
-        return (paste("ambigous",id,sep="_"))
+    } else {
+        message(paste("WARNING: ambigous gene id: ", id))
+        return(paste("ambigous", id, sep = "_"))
     }
 }
 
@@ -38,11 +38,11 @@ get_gene_name <- function(id, df){
 gtf.rtl <- rtracklayer::import(gtf)
 gtf.df <- as.data.frame(gtf.rtl)
 
-BPPARAM = MulticoreParam(workers=cores)
+BPPARAM <- MulticoreParam(workers = cores)
 
 # Importing counts
-samps <- read.table(file = gzfile(anname), header=TRUE, row.names=NULL)
-samps$sample_id <- paste(samps$sample_id, samps$condition, sep="_")
+samps <- read.table(file = gzfile(anname), header = TRUE, row.names = NULL)
+samps$sample_id <- paste(samps$sample_id, samps$condition, sep = "_")
 samps$condition <- factor(samps$condition)
 files <- file.path(samps$path, "quant.sf.gz")
 names(files) <- samps$sample_id
@@ -55,49 +55,46 @@ batches <- factor(samps$batch)
 comparisons <- strsplit(cmp, ",")
 
 ## check combi
-if (combi == "none"){
-    combi <- ''
+if (combi == "none") {
+    combi <- ""
 }
 
-txi <- tximport(files, type='salmon', txOut=TRUE, countsFromAbundance = "scaledTPM")
+txi <- tximport(files, type = "salmon", txOut = TRUE, countsFromAbundance = "scaledTPM")
 cts <- txi$counts
-cts <- cts[rowSums(cts) > 0,]
+cts <- cts[rowSums(cts) > 0, ]
 cts.original <- cts
 rownames(cts) <- sub("\\|.*", "", rownames(cts))
 
 # Transcript-to-gene mapping
-txdb.filename <- file.path(paste(gtf, "sqlite", sep="."))
-txdb <- makeTxDbFromGFF(gtf, format="gtf")
+txdb.filename <- file.path(paste(gtf, "sqlite", sep = "."))
+txdb <- makeTxDbFromGFF(gtf, format = "gtf")
 txdf <- select(txdb, keys(txdb, "GENEID"), "TXNAME", "GENEID")
 tab <- table(txdf$GENEID)
 txdf$ntx <- tab[match(txdf$GENEID, names(tab))]
 
 # DRIMSEQ
-#check for integrity -> define exception..
+# check for integrity -> define exception..
 all(rownames(cts) %in% txdf$TXNAME)
-txdf <- txdf[match(rownames(cts),txdf$TXNAME),]
+txdf <- txdf[match(rownames(cts), txdf$TXNAME), ]
 all(rownames(cts) == txdf$TXNAME)
 
-counts <- data.frame(gene_id=txdf$GENEID, feature_id=txdf$TXNAME, cts, check.names=FALSE)
-d <- dmDSdata(counts=counts, samples=samps)
+counts <- data.frame(gene_id = txdf$GENEID, feature_id = txdf$TXNAME, cts, check.names = FALSE)
+d <- dmDSdata(counts = counts, samples = samps)
 
 # Filter before running procedures:
 #   (1) it has a count of at least 10 in at least n.small samples
 #   (2) it has a relative abundance proportion of at least 0.1 in at least n.small samples
 #   (3) the total count of the corresponding gene is at least 10 in all n samples
 n <- nrow(samps)
-n.small <- n/length(levels(samps$condition))  # its not really the smallest group, needs to be improved
-d <- dmFilter(d,
-              min_samps_feature_expr=n.small, min_feature_expr=10,
-              min_samps_feature_prop=n.small, min_feature_prop=0.1,
-              min_samps_gene_expr=n, min_gene_expr=10)
+n.small <- n / length(levels(samps$condition)) # its not really the smallest group, needs to be improved
+d <- dmFilter(d, filter)
 
 ## shows how many of the remaining genes have N isoforms
-#table(table(counts(d)$gene_id))
+# table(table(counts(d)$gene_id))
 
 # create designmatrix
 #   original code for simple model
-design <- model.matrix(~groups, data=DRIMSeq::samples(d))
+design <- model.matrix(~groups, data = DRIMSeq::samples(d))
 
 ## name types and levels for design
 # bl <- sapply("batch", paste0, levels(batches)[-1])
@@ -126,56 +123,58 @@ comparison_objs <- list()
 setwd(outdir)
 
 ## Analyze according to comparison groups
-for(contrast in comparisons[[1]]){
+for (contrast in comparisons[[1]]) {
+    contrast_name <- strsplit(contrast, ":")[[1]][1]
+    contrast_groups <- strsplit(strsplit(contrast, ":")[[1]][2], "-vs-")
 
-    contrast_name <- strsplit(contrast,":")[[1]][1]
-    contrast_groups <- strsplit(strsplit(contrast,":")[[1]][2], "-vs-")
-
-    print(paste("Comparing ",contrast_name, sep=""))
+    print(paste("Comparing ", contrast_name, sep = ""))
 
     # determine contrast
     A <- strsplit(contrast_groups[[1]][1], "\\+")
     B <- strsplit(contrast_groups[[1]][2], "\\+")
-    minus <- 1/length(A[[1]])*(-1)
-    plus <- 1/length(B[[1]])
+    minus <- 1 / length(A[[1]]) * (-1)
+    plus <- 1 / length(B[[1]])
     contrast <- cbind(integer(dim(design)[2]), colnames(design))
-    for(i in A[[1]]){
-        contrast[which(contrast[,2]==i)]<- minus
+    for (i in A[[1]]) {
+        contrast[which(contrast[, 2] == i)] <- minus
     }
-    for(i in B[[1]]){
-        contrast[which(contrast[,2]==i)]<- plus
+    for (i in B[[1]]) {
+        contrast[which(contrast[, 2] == i)] <- plus
     }
-    contrast <- as.numeric(contrast[,1])
+    contrast <- as.numeric(contrast[, 1])
 
     # DEXSeq on DRIMSeq filtering
     sample.data <- DRIMSeq::samples(d)
-    count.data <- round(as.matrix(counts(d)[,-c(1:2)]))
+    count.data <- round(as.matrix(counts(d)[, -c(1:2)]))
     # The design formula of the DEXSeqDataSet here uses the language “exon” but this should be read as “transcript” for our analysis.
-    dxd <- DEXSeqDataSet(countData=count.data,
-                sampleData=sample.data,
-                design=~sample + exon + condition:exon,
-                featureID=counts(d)$feature_id,
-                groupID=counts(d)$gene_id
-            )
-    dxd <- DEXSeq::estimateSizeFactors(dxd) 
-    dxd <- DEXSeq::estimateDispersions(dxd, quiet=TRUE)
-    dxd <- testForDEU(dxd, reducedModel=~sample + exon)
+    dxd <- DEXSeqDataSet(
+        countData = count.data,
+        sampleData = sample.data,
+        design = ~ sample + exon + condition:exon,
+        featureID = counts(d)$feature_id,
+        groupID = counts(d)$gene_id
+    )
+    dxd <- DEXSeq::estimateSizeFactors(dxd)
+    dxd <- DEXSeq::estimateDispersions(dxd, quiet = TRUE)
+    dxd <- testForDEU(dxd, reducedModel = ~ sample + exon)
 
-    dxr <- DEXSeqResults(dxd, independentFiltering=FALSE)
+    dxr <- DEXSeqResults(dxd, independentFiltering = FALSE)
     qval <- perGeneQValue(dxr)
-    dxr.g <- data.frame(gene=names(qval),qval)
+    dxr.g <- data.frame(gene = names(qval), qval)
 
-    columns <- c("featureID","groupID","pvalue","padj")
-    dxr <- as.data.frame(dxr[,columns])
+    columns <- c("featureID", "groupID", "pvalue", "padj")
+    dxr <- as.data.frame(dxr[, columns])
 
-    dxr$Gene  <- lapply(dxr$groupID, function(x){get_gene_name(x,gtf.df)})
-    dxr <- dxr[,c(1,2,5,3,4)]
+    dxr$Gene <- lapply(dxr$groupID, function(x) {
+        get_gene_name(x, gtf.df)
+    })
+    dxr <- dxr[, c(1, 2, 5, 3, 4)]
     toprint <- as.data.frame(dxr1)
     toprint$transcripts <- vapply(toprint$transcripts, paste, collapse = ", ", character(1L))
-    #dxr <- apply(dxr, 2, as.character)
+    # dxr <- apply(dxr, 2, as.character)
 
-    write.table(toprint, gzfile(paste("Tables/DTU","DEXSEQ",combi,contrast_name,"table","results.tsv.gz",sep="_")), sep="\t", quote=F, row.names=FALSE)
-    
+    write.table(toprint, gzfile(paste("Tables/DTU", "DEXSEQ", combi, contrast_name, "table", "results.tsv.gz", sep = "_")), sep = "\t", quote = F, row.names = FALSE)
+
     # # stageR following DEXSeq
     # strp <- function(x) substr(x,1,15)
     # pConfirmation <- matrix(dxr$pvalue,ncol=1)
@@ -196,4 +195,4 @@ for(contrast in comparisons[[1]]){
     # write.table(as.data.frame(dex.padj), gzfile(paste("DTU_DEXSEQ",contrast_name,"stageR-filtered.tsv.gz",sep="_")), sep="\t", quote=F, row.names=FALSE)
 }
 
-save.image(file = paste("DTU_DEXSEQ", combi, "SESSION.gz",sep="_"), version = NULL, ascii = FALSE, compress = "gzip", safe = TRUE)
+save.image(file = paste("DTU_DEXSEQ", combi, "SESSION.gz", sep = "_"), version = NULL, ascii = FALSE, compress = "gzip", safe = TRUE)
