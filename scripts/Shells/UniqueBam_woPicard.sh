5bc78c5c26c7bfda040154f4c1a836311da7f441
Joerg Fallmann <jfallmann@users.noreply.github.com>
2024-04-11 21:44:40 +0200

    Hotfix qc (#59)
    * fastqc and multiqc updates
    
    * more fixes
    
    * quickfix minimap unique only MAPQ >= 60
    
    * quickfix unique bam

--- scripts/Shells/UniqueBam_woPicard.sh
+++ scripts/Shells/UniqueBam_woPicard.sh
@@ -3,19 +3,22 @@
 in=$1
 out=$2
 threads=$3
-bwa="${4:-}"
+specialmappers="${4:-}"
 
-samtools view -H ${in} | grep '@HD' > head
-samtools view -H ${in} | grep '@SQ' | sort -t$'\t' -k1,1 -k2,2V >> head
-samtools view -H ${in} | grep '@RG' >> head
-samtools view -H ${in} | grep '@PG' >> head
+samtools view -H ${in} | grep '@HD' > ${in}_head
+samtools view -H ${in} | grep '@SQ' | sort -t$'\t' -k1,1 -k2,2V >> ${in}_head
+samtools view -H ${in} | grep '@RG' >> ${in}_head
+samtools view -H ${in} | grep '@PG' >> ${in}_head
 
 
-if [[ "${in}" == *bwa* ]] || [[ -n "${bwa}" ]]
+if [[ "${in}" == *bwa* ]] || [[ "$specialmappers" == *bwa* ]]
 then
-    cat head <(samtools view --threads ${threads} -F 4 ${in} | grep -v "^@"| grep -v -e $'\t''XA:Z:' -e $'\t''SA:Z:') | samtools view --threads ${threads} -hb - > ${out}
+    cat ${in}_head <(samtools view --threads ${threads} -F 4 ${in} | grep -v "^@"| grep -v -e $'\t''XA:Z:' -e $'\t''SA:Z:') | samtools view --threads ${threads} -hb - > ${out}
+elif [[ "$1" == *minimap* ]] || [[ "$specialmappers" == *minimap* ]]
+then
+    cat ${in}_head <(samtools view --threads ${threads} -F 4 ${in} | grep -v "^@" | perl -wlane 'print if $F[4] >=60' | samtools view --threads ${threads} -hb - > ${out}
 else
-    cat head <(samtools view --threads ${threads} -F 4 ${in} | grep -v "^@" | grep -w -P "NH:i:1|tp:A:P") | samtools view --threads ${threads} -hb - > ${out}
+    cat ${in}_head <(samtools view --threads ${threads} -F 4 ${in} | grep -v "^@" | grep -w -P "NH:i:1|tp:A:P") | samtools view --threads ${threads} -hb - > ${out}
 fi
 
-rm -f head
+rm -f ${in}_head
