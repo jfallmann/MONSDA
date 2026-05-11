d0bfb4facf7d8d9f18c07cad031a63b7588eb14c
jfallmann <fallmann.joerg@gmail.com>
2026-05-11 13:12:57 +0200

    parallelized lane merge, cleanup

--- MONSDA/Logger.py
+++ MONSDA/Logger.py
@@ -1,47 +1,5 @@
 # Logger.py ---
-#
-# Filename: Logger.py
-# Description:
-# Author: Joerg Fallmann
-# Maintainer:
-# Created: Mon Aug 12 10:26:55 2019 (+0200)
-# Version:
-# Package-Requires: ()
-# Last-Updated: Wed Apr 29 16:42:40 2020 (+0200)
-#           By: Joerg Fallmann
-#     Update #: 91
-# URL:
-# Doc URL:
-# Keywords:
-# Compatibility:
-#
-#
 
-# Commentary:
-#
-#
-#
-#
-
-# Change Log:
-#
-#
-#
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or (at
-# your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
 
 # Code:
 import logging
--- MONSDA/Workflows.py
+++ MONSDA/Workflows.py
@@ -1,45 +1,4 @@
 # Workflows.py ---
-#
-# Filename: Workflows.py
-# Description:
-# Author: Joerg Fallmann
-# Maintainer:
-# Created: Tue Sep 18 15:39:06 2018 (+0200)
-# Version:
-# Package-Requires: ()
-# Last-Updated: Thu Feb  4 18:01:07 2021 (+0100)
-#           By: Joerg Fallmann
-#     Update #: 2888
-# URL:
-# Doc URL:
-# Keywords:
-# Compatibility:
-#
-#
-
-# Commentary:
-#
-#
-#
-
-# Change Log:
-#
-#
-#
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or (at
-# your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
 
 # Code:
 # import os, sys, inspect
